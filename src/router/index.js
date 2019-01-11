ed719a15f1342a713fabce8b95f3e4cc2710ee02
patfb <>
2019-01-11 17:01:59 -0500

    using a route guard

--- src/router/index.js
+++ src/router/index.js
@@ -48,7 +48,11 @@ export default new Router({
       path: "/parts/:partType/:id",
       name: "Parts",
       component: PartInfo,
-      props: true
+      props: true,
+      beforeEnter(to, from, next) {
+        const isValidId = Number.isInteger(Number(to.params.id));
+        next(isValidId);
+      }
     }
   ]
 });
