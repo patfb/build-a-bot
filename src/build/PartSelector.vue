f29849a7e0d8c9c4442606f4111fce0781b0747e
patfb <>
2019-01-12 11:42:15 -0500

    creating and using a custom directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -9,11 +9,13 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
+import pinDirective from "../shared/pin-directive";
+
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -25,6 +27,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  directives: { pin: pinDirective },
   props: {
     parts: { type: Array, required: true },
     position: {
@@ -79,9 +82,6 @@ export default {
   cursor: pointer;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
