e55e6d5285b28c5af4007bba2a7f91139650a5b3
patfb <>
2019-01-11 22:47:41 -0500

    using namespaced vuex modules

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -63,7 +63,7 @@ import CollapsibleSection from "../shared/CollapsibleSection.vue";
 export default {
   name: "RobotBuilder",
   created() {
-    this.$store.dispatch("getParts");
+    this.$store.dispatch("robots/getParts");
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -115,7 +115,7 @@ export default {
         robot.rightArm.cost +
         robot.base.cost;
       this.$store
-        .dispatch("addRobotToCart", Object.assign({}, robot, { cost }))
+        .dispatch("robots/addRobotToCart", Object.assign({}, robot, { cost }))
         .then(() => this.$router.push("/cart"));
       this.addedToCart = true;
     }
